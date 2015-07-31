2148ad5967d58c312ecbd2736f13d19074b5a124
Paul Larson <paul.larson@canonical.com>
2015-07-31 10:57:06 -0500

    Add a helper function for reading the spi test_opportunity data

--- snappy_device_agents/__init__.py
+++ snappy_device_agents/__init__.py
@@ -11,3 +11,26 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import json
+
+
+def get_test_opportunity(spi_file='spi_test_opportunity.json'):
+    with open(spi_file, encoding='utf-8') as spi_json:
+        test_opportunity = json.load(spi_json)
+    # test_payload and image_reference may contain json in a string
+    # XXX: This can be removed in the future when arbitrary json is
+    # supported
+    try:
+        test_opportunity['test_payload'] = json.loads(
+            test_opportunity['test_payload'])
+    except:
+        # If this fails, we simply leave the field alone
+        pass
+    try:
+        test_opportunity['image_reference'] = json.loads(
+            test_opportunity['image_reference'])
+    except:
+        # If this fails, we simply leave the field alone
+        pass
+    return test_opportunity
