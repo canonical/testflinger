5d862227cd7fb49fd063f6027195d29b6ce0ae92
Paul Larson <paul.larson@canonical.com>
2016-08-17 16:00:03 -0500

    Run test phases as defined by the config

--- testflinger_agent/client.py
+++ testflinger_agent/client.py
@@ -13,7 +13,11 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
+import json
+import os
 import requests
+import subprocess
+import sys
 import time
 
 from urllib.parse import urljoin
@@ -25,10 +29,20 @@ logger = logging.getLogger()
 
 def process_jobs():
     """Coordinate checking for new jobs and handling them if they exists"""
+    TEST_PHASES = ['setup', 'provision', 'test']
     job_data = check_jobs()
     if not job_data:
         return
     logger.info("Starting job %s", job_data.get('job_id'))
+    rundir = os.path.join(testflinger_agent.config.get('execution_basedir'),
+                          job_data.get('job_id'))
+    os.makedirs(rundir)
+    # Dump the job data to testflinger.json in our execution directory
+    with open(os.path.join(rundir, 'testflinger.json'), 'w') as f:
+        json.dump(job_data, f)
+
+    for phase in TEST_PHASES:
+        run_test_phase(phase, rundir)
 
 
 def check_jobs():
@@ -53,3 +67,29 @@ def check_jobs():
         logger.exception(e)
         # Wait a little extra before trying again
         time.sleep(60)
+
+
+def run_test_phase(phase, rundir):
+    cmd = testflinger_agent.config.get(phase+'_command')
+    if not cmd:
+        return
+    phase_log = os.path.join(rundir, phase+'.log')
+    logger.info('Running %s_command: %s' % (phase, cmd))
+    run_with_log(cmd, phase_log, rundir)
+
+
+def run_with_log(cmd, logfile, cwd=None):
+    with open(logfile, 'w') as f:
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                                   stderr=subprocess.STDOUT,
+                                   shell=True, cwd=cwd)
+        while process.poll() is None:
+            line = process.stdout.readline()
+            if line:
+                sys.stdout.write(line.decode())
+                f.write(line.decode())
+                f.flush()
+        line = process.stdout.read()
+        if line:
+            sys.stdout.write(line.decode())
+            f.write(line.decode())
--- testflinger_agent/tests/test_client.py
+++ testflinger_agent/tests/test_client.py
@@ -14,6 +14,9 @@
 
 import json
 import requests
+import tempfile
+import os
+import shutil
 import uuid
 
 import testflinger_agent
@@ -38,3 +41,60 @@ class ClientTest(TestCase):
         mock_requests_get.return_value = fake_response
         job_data = testflinger_agent.client.check_jobs()
         self.assertEqual(job_data, fake_job_data)
+
+
+class ClientRunTests(TestCase):
+    def setUp(self):
+        self.tmpdir = tempfile.mkdtemp()
+        testflinger_agent.config = {'agent_id': 'test01',
+                                    'polling_interval': '2',
+                                    'server_address': '127.0.0.1:8000',
+                                    'job_queues': ['test'],
+                                    'execution_basedir': self.tmpdir,
+                                    'logging_basedir': self.tmpdir,
+                                    }
+
+    def tearDown(self):
+        shutil.rmtree(self.tmpdir)
+
+    @patch('requests.get')
+    def test_check_and_run_setup(self, mock_requests_get):
+        testflinger_agent.config['setup_command'] = 'echo setup1'
+        fake_job_data = {'job_id': str(uuid.uuid1()),
+                         'job_queue': 'test'}
+        fake_response = requests.Response()
+        fake_response._content = json.dumps(fake_job_data).encode()
+        mock_requests_get.return_value = fake_response
+        testflinger_agent.client.process_jobs()
+        setuplog = open(os.path.join(self.tmpdir,
+                                     fake_job_data.get('job_id'),
+                                     'setup.log')).read()
+        self.assertEqual('setup1', setuplog.strip())
+
+    @patch('requests.get')
+    def test_check_and_run_provision(self, mock_requests_get):
+        testflinger_agent.config['provision_command'] = 'echo provision1'
+        fake_job_data = {'job_id': str(uuid.uuid1()),
+                         'job_queue': 'test'}
+        fake_response = requests.Response()
+        fake_response._content = json.dumps(fake_job_data).encode()
+        mock_requests_get.return_value = fake_response
+        testflinger_agent.client.process_jobs()
+        provisionlog = open(os.path.join(self.tmpdir,
+                                         fake_job_data.get('job_id'),
+                                         'provision.log')).read()
+        self.assertEqual('provision1', provisionlog.strip())
+
+    @patch('requests.get')
+    def test_check_and_run_test(self, mock_requests_get):
+        testflinger_agent.config['test_command'] = 'echo test1'
+        fake_job_data = {'job_id': str(uuid.uuid1()),
+                         'job_queue': 'test'}
+        fake_response = requests.Response()
+        fake_response._content = json.dumps(fake_job_data).encode()
+        mock_requests_get.return_value = fake_response
+        testflinger_agent.client.process_jobs()
+        testlog = open(os.path.join(self.tmpdir,
+                                    fake_job_data.get('job_id'),
+                                    'test.log')).read()
+        self.assertEqual('test1', testlog.strip())
