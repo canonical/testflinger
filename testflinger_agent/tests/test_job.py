eb9be19d543b28c60ab68904699fd7fd87fe1ea5
Paul Larson <paul.larson@canonical.com>
2017-11-28 02:12:50 -0600

    Eliminate errors when running the unittests coming from python requests

--- testflinger_agent/tests/test_job.py
+++ testflinger_agent/tests/test_job.py
@@ -2,7 +2,7 @@ import os
 import shutil
 import tempfile
 
-from mock import patch
+from mock import MagicMock
 from unittest import TestCase
 
 import testflinger_agent
@@ -69,7 +69,7 @@ class JobTests(TestCase):
         logfile = os.path.join(self.tmpdir, 'testlog')
         client = TestflingerClient(self.config)
         fake_job_data = {'global_timeout': 1}
-        patch('client.post_live_output')
+        client.post_live_output = MagicMock()
         job = TestflingerJob(fake_job_data, client)
         job.phase = 'test'
         job.run_with_log('sleep 3', logfile)
@@ -84,7 +84,7 @@ class JobTests(TestCase):
         self.config['global_timeout'] = 1
         client = TestflingerClient(self.config)
         fake_job_data = {'global_timeout': 3}
-        patch('client.post_live_output')
+        client.post_live_output = MagicMock()
         job = TestflingerJob(fake_job_data, client)
         job.phase = 'test'
         job.run_with_log('sleep 3', logfile)
@@ -98,7 +98,7 @@ class JobTests(TestCase):
         logfile = os.path.join(self.tmpdir, 'testlog')
         client = TestflingerClient(self.config)
         fake_job_data = {'output_timeout': 1}
-        patch('client.post_live_output')
+        client.post_live_output = MagicMock()
         job = TestflingerJob(fake_job_data, client)
         job.phase = 'test'
         # unfortunately, we need to sleep for longer that 10 seconds here
@@ -115,7 +115,7 @@ class JobTests(TestCase):
         self.config['output_timeout'] = 1
         client = TestflingerClient(self.config)
         fake_job_data = {'output_timeout': 30}
-        patch('client.post_live_output')
+        client.post_live_output = MagicMock()
         job = TestflingerJob(fake_job_data, client)
         job.phase = 'test'
         # unfortunately, we need to sleep for longer that 10 seconds here
@@ -131,7 +131,7 @@ class JobTests(TestCase):
         logfile = os.path.join(self.tmpdir, 'testlog')
         client = TestflingerClient(self.config)
         fake_job_data = {'output_timeout': 1}
-        patch('client.post_live_output')
+        client.post_live_output = MagicMock()
         job = TestflingerJob(fake_job_data, client)
         job.phase = 'provision'
         # unfortunately, we need to sleep for longer that 10 seconds here
