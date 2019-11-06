32eeda5a1ba05d5e04bf31fdc255bd3a6e63c04d
Paul Larson <paul.larson@canonical.com>
2019-11-06 11:27:50 -0600

    Move some maas2 device config data into instance scope

--- devices/maas2/maas2.py
+++ devices/maas2/maas2.py
@@ -36,6 +36,10 @@ class Maas2:
             self.config = yaml.safe_load(configfile)
         with open(job_data) as j:
             self.job_data = json.load(j)
+        self.maas_user = self.config.get('maas_user')
+        self.node_id = self.config.get('node_id')
+        self.agent_name = self.config.get('agent_name')
+        self.timeout_min = int(self.config.get('timeout_min', 60))
 
     def _logger_debug(self, message):
         logger.debug("MAAS: {}".format(message))
@@ -53,26 +57,21 @@ class Maas2:
         logger.critical("MAAS: {}".format(message))
 
     def recover(self):
-        agent_name = self.config.get('agent_name')
-        self._logger_info("Releasing node {}".format(agent_name))
+        self._logger_info("Releasing node {}".format(self.agent_name))
         self.node_release()
 
     def provision(self):
-        maas_user = self.config.get('maas_user')
-        node_id = self.config.get('node_id')
-        agent_name = self.config.get('agent_name')
-        timeout_min = self.config.get('timeout_min')
         provision_data = self.job_data.get('provision_data')
         # Default to a safe LTS if no distro is specified
         distro = provision_data.get('distro', 'xenial')
         self._logger_info('Acquiring node')
-        cmd = ['maas', maas_user, 'machines', 'allocate',
-               'system_id={}'.format(node_id)]
+        cmd = ['maas', self.maas_user, 'machines', 'allocate',
+               'system_id={}'.format(self.node_id)]
         # Do not use runcmd for this - we need the output, not the end user
         subprocess.check_call(cmd)
         self._logger_info('Starting node {} '
-                          'with distro {}'.format(agent_name, distro))
-        cmd = ['maas', maas_user, 'machine', 'deploy', node_id,
+                          'with distro {}'.format(self.agent_name, distro))
+        cmd = ['maas', self.maas_user, 'machine', 'deploy', self.node_id,
                'distro_series={}'.format(distro)]
         kernel = provision_data.get('kernel')
         if kernel:
@@ -92,10 +91,9 @@ class Maas2:
 
         # Make sure the device is available before returning
         minutes_spent = 0
-        if timeout_min is None:
-            timeout_min = 60
-        self._logger_info("Timeout value: {} minutes.".format(timeout_min))
-        while minutes_spent < timeout_min:
+        self._logger_info("Timeout value: {} minutes.".format(
+            self.timeout_min))
+        while minutes_spent < self.timeout_min:
             time.sleep(60)
             minutes_spent += 1
             self._logger_info('{} minutes passed '
@@ -114,12 +112,12 @@ class Maas2:
                     self._logger_info('Deployed and booted.')
                     return
 
-        self._logger_error('Device {} still in "{}" state, '
-                           'deployment failed!'.format(agent_name, status))
+        self._logger_error('Device {} still in "{}" state, deployment '
+                           'failed!'.format(self.agent_name, status))
         self._logger_error(process.stdout.decode())
         exception_msg = "Provisioning failed because deployment timeout. " + \
                         "Deploying for more than " + \
-                        "{} minutes.".format(timeout_min)
+                        "{} minutes.".format(self.timeout_min)
         raise ProvisioningError(exception_msg)
 
     def check_test_image_booted(self):
@@ -131,7 +129,7 @@ class Maas2:
         try:
             subprocess.check_output(
                 cmd, stderr=subprocess.STDOUT, timeout=60)
-        except:
+        except Exception:
             return False
         # If we get here, then the above command proved we are booted
         return True
@@ -144,9 +142,7 @@ class Maas2:
         Deploying: Deployment in progress
         Deployed: Node is provisioned and ready for use
         """
-        maas_user = self.config.get('maas_user')
-        node_id = self.config.get('node_id')
-        cmd = ['maas', maas_user, 'machine', 'read', node_id]
+        cmd = ['maas', self.maas_user, 'machine', 'read', self.node_id]
         # Do not use runcmd for this - we need the output, not the end user
         output = subprocess.check_output(cmd)
         data = json.loads(output.decode())
@@ -154,9 +150,7 @@ class Maas2:
 
     def node_release(self):
         """Release the node to make it available again"""
-        maas_user = self.config.get('maas_user')
-        node_id = self.config.get('node_id')
-        cmd = ['maas', maas_user, 'machine', 'release', node_id]
+        cmd = ['maas', self.maas_user, 'machine', 'release', self.node_id]
         subprocess.run(cmd)
         # Make sure the device is available before returning
         for timeout in range(0, 10):
@@ -164,7 +158,6 @@ class Maas2:
             status = self.node_status()
             if status == 'Ready':
                 return
-        agent_name = self.config.get('agent_name')
-        self._logger_error('Device {} still in "{}" state, '
-                           'could not recover!'.format(agent_name, status))
+        self._logger_error('Device {} still in "{}" state, could not '
+                           'recover!'.format(self.agent_name, status))
         raise RecoveryError("Device recovery failed!")
