5a94e19b41ce0449f927d6c8729cd395311a7927
Paul Larson <paul.larson@canonical.com>
2023-02-17 09:42:16 -0600

    Convert to using pyproject.toml for setup

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (C) 2015 Canonical
+# Copyright (C) 2015-2023 Canonical
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -13,38 +13,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
-import sys
+from setuptools import setup
 
-from setuptools import (
-    find_packages,
-    setup,
-)
-
-assert sys.version_info >= (3,), "Python 3 is required"
-
-
-VERSION = "0.0.1"
-
-datafiles = [
-    (d, [os.path.join(d, f) for f in files])
-    for d, folders, files in os.walk("data")
-]
-
-setup(
-    name="snappy-device-agents",
-    version=VERSION,
-    description=(
-        "Device agents scripts for provisioning and running "
-        "tests on Snappy devices"
-    ),
-    author="Snappy Device Agents Developers",
-    author_email="paul.larson@canonical.com",
-    url="https://launchpad.net/snappy-device-agents",
-    license="GPLv3",
-    packages=find_packages(),
-    data_files=datafiles,
-    setup_requires=["pytest-runner"],
-    install_requires=["PyYAML>=3.11"],
-    scripts=["snappy-device-agent"],
-)
+setup()
