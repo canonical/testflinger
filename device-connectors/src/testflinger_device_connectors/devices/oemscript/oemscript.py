dd7b7cbdcbdb9e20ae9308d447c5fa4f690deb72
nancyc12 <nancy.chen@canonical.com>
2024-02-29 23:54:06 +0800

    refine to ensure image removal happen even in an error situation

--- device-connectors/src/testflinger_device_connectors/devices/oemscript/oemscript.py
+++ device-connectors/src/testflinger_device_connectors/devices/oemscript/oemscript.py
@@ -97,15 +97,16 @@ class OemScript:
                 "Please provide an image 'url' in the provision_data section"
             )
             raise ProvisioningError("No image url provided")
-        image_file = download(image_url)
+        try:
+            image_file = download(image_url)
 
-        self.run_recovery_script(image_file)
+            self.run_recovery_script(image_file)
 
-        self.check_device_booted()
-
-        # remove the .iso image
-        cmd = f"rm -f {image_file}"
-        subprocess.check_output(cmd, stderr=subprocess.STDOUT, timeout=60)
+            self.check_device_booted()
+        finally:
+            # remove the .iso image
+            if image_file:
+                os.unlink(image_file)
 
     def run_recovery_script(self, image_file):
         """Download and run the OEM recovery script"""
