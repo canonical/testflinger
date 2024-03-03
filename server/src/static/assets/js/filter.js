bc77314d5a816d40a590962d4b845077c2207da5
Paul Larson <paul.larson@canonical.com>
2024-03-03 07:23:59 -0600

    Restrict search rows to a "searchable-row" class

--- server/src/static/assets/js/filter.js
+++ server/src/static/assets/js/filter.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const searchBox = document.querySelector('.p-search-box__input');
-    const tableRows = Array.from(document.querySelectorAll('.p-table--mobile-card tbody tr'));
+    const tableRows = Array.from(document.querySelectorAll('.p-table--mobile-card tbody tr.searchable-row'));
 
     searchBox.addEventListener('input', function() {
         const searchTerm = searchBox.value.toLowerCase();
