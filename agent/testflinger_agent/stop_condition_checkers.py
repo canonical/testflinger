5b252481cc53285b5a1524388beb9430a47b8f54
Paul Larson <paul.larson@canonical.com>
2024-03-17 20:56:05 -0500

    Add checks for conditions that should cause the job to stop execution

---
+++ agent/testflinger_agent/stop_condition_checkers.py
@@ -0,0 +1,51 @@
+# Copyright (C) 2024 Canonical
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+import time
+from typing import Optional
+
+from .client import TestflingerClient
+
+
+class JobCancelledChecker:
+    def __init__(self, client: TestflingerClient, job_id: str):
+        self.client = client
+        self.job_id = job_id
+
+    def __call__(self) -> Optional[str]:
+        if self.client.check_job_state(self.job_id) == "cancelled":
+            return "\nJob cancellation was requested, exiting.\n"
+        return None
+
+
+class GlobalTimeoutChecker:
+    def __init__(self, timeout: int):
+        self.timeout = timeout
+        self.start_time = time.time()
+
+    def __call__(self) -> Optional[str]:
+        if time.time() - self.start_time > self.timeout:
+            return f"\nERROR: Global timeout reached! ({self.timeout}s)\n"
+        return None
+
+
+class OutputTimeoutChecker:
+    def __init__(self, timeout: int):
+        self.timeout = timeout
+        self.start_time = time.time()
+
+    def __call__(self) -> Optional[str]:
+        if time.time() - self.start_time > self.timeout:
+            return f"\nERROR: Output timeout reached! ({self.timeout}s)\n"
+        return None
---
+++ agent/testflinger_agent/tests/test_stop_condition_checkers.py
@@ -0,0 +1,53 @@
+# Copyright (C) 2024 Canonical
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+import time
+
+from ..client import TestflingerClient
+from ..stop_condition_checkers import (
+    JobCancelledChecker,
+    GlobalTimeoutChecker,
+    OutputTimeoutChecker,
+)
+
+
+class TestStopConditionCheckers:
+    def test_job_cancelled_checker(self, mocker):
+        """Test that the job cancelled checker detects cancelled state"""
+        client = TestflingerClient({"server_address": "http://localhost"})
+        checker = JobCancelledChecker(client, "job_id")
+
+        # Nothing should happen if the job is not cancelled
+        mocker.patch.object(client, "check_job_state", return_value="test")
+        assert checker() is None
+
+        # If the job is cancelled, the checker should return a message
+        mocker.patch.object(
+            client, "check_job_state", return_value="cancelled"
+        )
+        assert "Job cancellation was requested, exiting." in checker()
+
+    def test_global_timeout_checker(self):
+        """Test that the global timeout checker works as expected."""
+        checker = GlobalTimeoutChecker(0.5)
+        assert checker() is None
+        time.sleep(0.6)
+        assert "ERROR: Global timeout reached! (0.5s)" in checker()
+
+    def test_output_timeout_checker(self):
+        """Test that the output timeout checker works as expected."""
+        checker = OutputTimeoutChecker(0.5)
+        assert checker() is None
+        time.sleep(0.6)
+        assert "ERROR: Output timeout reached! (0.5s)" in checker()
