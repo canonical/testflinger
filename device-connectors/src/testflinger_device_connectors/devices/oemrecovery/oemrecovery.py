d96f4ffebfb279987d467eac7f2d74aa57390bdd
Kevin Yeh <kevin.yeh@canonical.com>
2024-03-19 18:43:39 +0800

    Update: add a timeout to subprocess call.

--- device-connectors/src/testflinger_device_connectors/devices/oemrecovery/oemrecovery.py
+++ device-connectors/src/testflinger_device_connectors/devices/oemrecovery/oemrecovery.py
@@ -112,6 +112,7 @@ class OemRecovery:
                     self.config["device_ip"],
                 ),
                 shell=True,
+                timeout=5
             )
         ).decode()
 
@@ -146,6 +147,7 @@ class OemRecovery:
                     self.config["device_ip"],
                 ),
                 shell=True,
+                timeout=5
             )
         ).decode()
 
