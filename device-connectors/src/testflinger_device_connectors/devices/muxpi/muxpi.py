04c636f780d8d06e7cb36c5a5794115320973bdf
Remy MARTIN <remy.martin@canonical.com>
2024-06-28 18:49:34 +0200

    Fix media parameter issue for muxpi (#296)
    media parameter is designed for a DUT installed with zapper.\nIf the parameter is used, the TF agent will start by switching back to zapper the 2 devices that could host an image (USB and SD), to avoid having 2 images installed at a time on the same DUT. However if sdwire isn't connected to zapper, the typecmux device node won't get detected, leading to a job failure

--- device-connectors/src/testflinger_device_connectors/devices/muxpi/muxpi.py
+++ device-connectors/src/testflinger_device_connectors/devices/muxpi/muxpi.py
@@ -195,12 +195,15 @@ class MuxPi:
             self._run_control(cmd)
         else:
             media = self.job_data["provision_data"]["media"]
+            # If media option is provided, then DUT is probably capable of
+            # booting from different media, we should switch both of them
+            # to TS side regardless of which one was previously used
             try:
-                # If media option is provided, then DUT is probably capable of
-                # booting from different media, we should switch both of them
-                # to TS side regardless of which one was previously used
                 cmd = "zapper sdwire plug_to_self"
                 sd_node = self._run_control(cmd)
+            except Exception:
+                pass
+            try:
                 cmd = "zapper typecmux plug_to_self"
                 usb_node = self._run_control(cmd)
             except Exception:
