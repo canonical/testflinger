8db00b671ef9d733635abf9e7d002bcf7960585e
Paul Larson <paul.larson@canonical.com>
2024-08-11 12:50:46 -0500

    Small fixes for the agent-host charm tests

--- agent/charms/testflinger-agent-host-charm/tests/test_charm.py
+++ agent/charms/testflinger-agent-host-charm/tests/test_charm.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Canonical
+# Copyright 2024 Canonical
 # See LICENSE file for licensing details.
 #
 # Learn more about testing at: https://juju.is/docs/sdk/testing
@@ -41,25 +41,20 @@ class TestCharm(unittest.TestCase):
         self.assertEqual(mock_write_file.call_count, 2)
 
     @patch("os.listdir")
-    @patch("os.remove")
     @patch("shutil.copy")
     @patch("os.chmod")
     def test_update_tf_cmd_scripts(
-        self, mock_chmod, mock_copy, mock_remove, mock_listdir
+        self, mock_chmod, mock_copy, mock_listdir
     ):
         """Test the update_tf_cmd_scripts method"""
         charm = self.harness.charm
-        usr_local_bin_files = ["tf-script1", "tf-script2", "other-file"]
         tf_cmd_scripts_files = ["tf-script3", "tf-script4"]
 
-        mock_listdir.side_effect = [usr_local_bin_files, tf_cmd_scripts_files]
+        mock_listdir.side_effect = [tf_cmd_scripts_files]
 
         charm.update_tf_cmd_scripts()
         tf_cmd_dir = "src/tf-cmd-scripts/"
         usr_local_bin = "/usr/local/bin/"
-        mock_remove.assert_any_call(os.path.join(usr_local_bin, "tf-script1"))
-        mock_remove.assert_any_call(os.path.join(usr_local_bin, "tf-script2"))
-        self.assertEqual(mock_remove.call_count, 2)
         mock_copy.assert_any_call(
             os.path.join(tf_cmd_dir, "tf-script3"),
             usr_local_bin,
