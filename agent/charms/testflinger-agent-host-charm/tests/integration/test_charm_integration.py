a0e314331e060397e2b259814d2295c229e95f25
Paul Larson <paul.larson@canonical.com>
2024-08-15 08:35:16 -0500

    cleanup

--- agent/charms/testflinger-agent-host-charm/tests/integration/test_charm_integration.py
+++ agent/charms/testflinger-agent-host-charm/tests/integration/test_charm_integration.py
@@ -7,23 +7,20 @@ CHARM_PATH = Path(__file__).parent.parent.parent
 APP_NAME = "testflinger-agent-host"
 
 
-#@pytest.mark.skip_if_deployed
-#@pytest.mark.setup
 @pytest.mark.abort_on_fail
 async def test_build_and_deploy(ops_test: OpsTest):
     charm = await ops_test.build_charm(CHARM_PATH)
     app = await ops_test.model.deploy(charm)
 
-    await ops_test.model.wait_for_idle(status="active",  timeout=600)
+    await ops_test.model.wait_for_idle(status="active", timeout=600)
+    assert app.status == "active"
 
 
-#@pytest.mark.skip_if_deployed
-#@pytest.mark.abort_on_fail
 async def test_action_update_testflinger(ops_test: OpsTest):
-    #charm = await ops_test.build_charm(CHARM_PATH)
-    #app = await ops_test.model.deploy(charm)
-    await ops_test.model.wait_for_idle(status="active",  timeout=600)
-    action = await ops_test.model.units.get(f"{APP_NAME}/0").run_action("update-testflinger")
+    await ops_test.model.wait_for_idle(status="active", timeout=600)
+    action = await ops_test.model.units.get(f"{APP_NAME}/0").run_action(
+        "update-testflinger"
+    )
     await action.wait()
     assert action.status == "completed"
     assert action.results["return-code"] == 0
