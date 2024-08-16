87bc137b03104c42ffe75b6545396c538bb0b285
Paul Larson <paul.larson@canonical.com>
2024-08-16 09:25:57 -0500

    Shared agent code location (#329)
    * Move run_with_logged_errors out of charm.py
    
    * Update testflinger source and add action for it
    
    * Add integration tests and docs for the update-testflinger action
    
    * cleanup

---
+++ agent/charms/testflinger-agent-host-charm/src/defaults.py
@@ -0,0 +1,7 @@
+# Copyright 2024 Canonical
+# See LICENSE file for licensing details.
+
+DEFAULT_TESTFLINGER_REPO = "https://github.com/canonical/testflinger.git"
+DEFAULT_BRANCH = "main"
+LOCAL_TESTFLINGER_PATH = "/srv/testflinger"
+VIRTUAL_ENV_PATH = "/srv/testflinger-venv"
