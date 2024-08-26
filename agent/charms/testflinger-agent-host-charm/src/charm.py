f5f12757b5ff0aabcdab2519148e5a820b5f411c
Paul Larson <paul.larson@canonical.com>
2024-08-26 12:56:26 -0500

    Add update-configs action (#337)

--- agent/charms/testflinger-agent-host-charm/src/charm.py
+++ agent/charms/testflinger-agent-host-charm/src/charm.py
@@ -6,10 +6,13 @@
 
 
 import logging
-import shutil
 import os
-from pathlib import PosixPath
+import shutil
+import sys
+from pathlib import Path
 from common import run_with_logged_errors
+from git import Repo, GitCommandError
+from jinja2 import Template
 
 from charms.operator_libs_linux.v0 import apt
 from ops.charm import CharmBase
@@ -22,7 +25,7 @@ from ops.model import (
     ModelError,
 )
 import testflinger_source
-from defaults import LOCAL_TESTFLINGER_PATH
+from defaults import AGENT_CONFIGS_PATH, LOCAL_TESTFLINGER_PATH
 
 logger = logging.getLogger(__name__)
 
@@ -39,6 +42,10 @@ class TestflingerAgentHostCharm(CharmBase):
         self.framework.observe(self.on.config_changed, self.on_config_changed)
         self.framework.observe(self.on.upgrade_charm, self.on_upgrade_charm)
         self.framework.observe(
+            self.on.update_configs_action,
+            self.on_update_configs_action,
+        )
+        self.framework.observe(
             self.on.update_testflinger_action,
             self.on_update_testflinger_action,
         )
@@ -53,6 +60,13 @@ class TestflingerAgentHostCharm(CharmBase):
         self.setup_docker()
         self.update_tf_cmd_scripts()
         self.update_testflinger_repo()
+        try:
+            self.update_config_files()
+        except ValueError:
+            self.unit.status = BlockedStatus(
+                "config-repo and config-dir must be set"
+            )
+            return
 
     def install_dependencies(self):
         """Install the packages needed for the agent"""
@@ -78,6 +92,39 @@ class TestflingerAgentHostCharm(CharmBase):
         testflinger_source.create_virtualenv()
         self.unit.status = MaintenanceStatus("Cloning testflinger repo")
         testflinger_source.clone_repo(LOCAL_TESTFLINGER_PATH)
+
+    def update_config_files(self):
+        """
+        Clone the config files from the repo and swap it in for whatever is
+        in AGENT_CONFIGS_PATH
+        """
+        config_repo = self.config.get("config-repo")
+        config_dir = self.config.get("config-dir")
+        config_branch = self.config.get("config-branch")
+        if not config_repo or not config_dir:
+            logger.error("config-repo and config-dir must be set")
+            raise ValueError("config-repo and config-dir must be set")
+        tmp_repo_path = Path("/srv/tmp-agent-configs")
+        repo_path = Path(AGENT_CONFIGS_PATH)
+        if tmp_repo_path.exists():
+            shutil.rmtree(tmp_repo_path, ignore_errors=True)
+        try:
+            Repo.clone_from(
+                url=config_repo,
+                branch=config_branch,
+                to_path=tmp_repo_path,
+                depth=1,
+            )
+        except GitCommandError:
+            logger.exception("Failed to update config files")
+            self.unit.status = BlockedStatus(
+                "Failed to update or config files"
+            )
+            sys.exit(1)
+
+        if repo_path.exists():
+            shutil.rmtree(repo_path, ignore_errors=True)
+        shutil.move(tmp_repo_path, repo_path)
 
     def setup_docker(self):
         run_with_logged_errors(["groupadd", "docker"])
@@ -122,6 +169,13 @@ class TestflingerAgentHostCharm(CharmBase):
     def on_config_changed(self, _):
         self.unit.status = MaintenanceStatus("Handling config_changed hook")
         self.copy_ssh_keys()
+        try:
+            self.update_config_files()
+        except ValueError:
+            self.unit.status = BlockedStatus(
+                "config-repo and config-dir must be set"
+            )
+            return
         self.unit.status = ActiveStatus()
 
     def install_apt_packages(self, packages: list):
@@ -140,8 +194,22 @@ class TestflingerAgentHostCharm(CharmBase):
 
     def on_update_testflinger_action(self, event):
         """Update Testflinger agent code"""
-        self.unit.status = MaintenanceStatus("Handling update action")
+        self.unit.status = MaintenanceStatus("Updating Testflinger Agent Code")
         self.update_testflinger_repo()
+        self.unit.status = ActiveStatus()
+
+    def on_update_configs_action(self, event):
+        """Update agent configs"""
+        self.unit.status = MaintenanceStatus(
+            "Updating Testflinger Agent Configs"
+        )
+        try:
+            self.update_config_files()
+        except ValueError:
+            self.unit.status = BlockedStatus(
+                "config-repo and config-dir must be set"
+            )
+            return
         self.unit.status = ActiveStatus()
 
 
