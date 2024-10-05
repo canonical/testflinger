4b97e1877217597f511015eced180b585c3f43af
Paul Larson <paul.larson@canonical.com>
2024-10-05 14:55:59 -0500

    Separate variables from main.tf

--- agent/terraform/main.tf
+++ agent/terraform/main.tf
@@ -1,39 +1,3 @@
-
-variable "juju_model" {
-  type        = string
-  description = "Name of the Juju model"
-}
-
-variable "agent_host_name" {
-  type        = string
-  description = "Name of the agent host juju application"
-}
-
-variable "config_repo" {
-  type        = string
-  description = "Repository URL for the agent configs on this agent host"
-}
-
-variable "config_branch" {
-  type        = string
-  description = "Repository branch for the agent configs"
-}
-
-variable "config_dir" {
-  type        = string
-  description = "Directory within the config repo containing the charm configuration"
-}
-
-variable "ssh_public_key" {
-  type        = string
-  description = "base64 encoded ssh public key to use on the agent host"
-}
-
-variable "ssh_private_key" {
-  type        = string
-  description = "base64 encoded ssh private key to use on the agent host"
-}
-
 resource "juju_application" "testflinger-agent-host" {
   name  = var.agent_host_name
   model = var.juju_model
