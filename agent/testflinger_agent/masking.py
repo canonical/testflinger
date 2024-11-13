27d1da4a314a163a5ed27a977d6bd6a41e7a4a47
George Boukeas <george.boukeas@canonical.com>
2024-11-13 12:50:08 +0000

    feat(agent): introduce dynamic masker for environment variables

--- agent/testflinger_agent/masking.py
+++ agent/testflinger_agent/masking.py
@@ -7,6 +7,7 @@ the same sensitive information is always replaced by the same mask.
 """
 
 import hashlib
+import os
 import re
 from typing import List, Optional
 
@@ -23,20 +24,83 @@ class Masker:
 
     def __init__(self, patterns: List[str], hash_length: Optional[int] = None):
         # join all patterns into a single combined pattern and compile it
-        combined = "|".join(f"({pattern})" for pattern in patterns)
+        combined = self.combine(pattern for pattern in patterns if pattern)
+        if not combined:
+            raise ValueError("Empty combined pattern")
         self.pattern = re.compile(combined)
         # the length of the hash to be used for masking
         # (a value of `None` results in the full hash length)
         self.hash_length = hash_length
 
-    def hash(self, text: str) -> str:
-        """Return a hash of appropriate length for the giver `text`"""
-        return hashlib.sha256(text.encode()).hexdigest()[: self.hash_length]
+    @staticmethod
+    def combine(patterns: List[str]) -> str:
+        """Return a single disjunctive regular expression from `patterns`"""
+        return "|".join(f"({pattern})" for pattern in patterns)
+
+    @staticmethod
+    def hash(text: str) -> str:
+        """Return a hash for `text`"""
+        return hashlib.sha256(text.encode()).hexdigest()
 
     def mask(self, match: re.Match) -> str:
         """Return a string with a mask applied on a pattern `match`"""
-        return f"**{self.hash(match.group())}**"
+        return f"**{self.hash(match.group())[:self.hash_length]}**"
 
     def apply(self, text: str) -> str:
         """Return a string with all pattern matches in `text` masked"""
         return self.pattern.sub(self.mask, text)
+
+
+class DynamicMasker(Masker):
+    """
+    A class for masking sensitive information in text.
+
+    In addition to sensitive patterns, the dynamic masker can also be provided
+    with a list of sensitive environment variables. Masking will also be
+    performed on the *values* of these environment variables. This masker is
+    called "dynamic" because the values of environment variables are not known
+    in advance (when instantiating the masker) but are retrieved when the
+    masker's `apply` method is called.
+
+    Example:
+        >>> os.environ["SECRET"] = "123-45-6789"
+        >>> masker = DynamicMasker(variables=["SECRET"], hash_length=6)
+        >>> masker.apply("SSN: 123-45-6789")
+        'SSN: **01a546**'
+        >>> masker.apply(f"SSN: {os.environ['SECRET']}")
+        'SSN: **01a546**'
+    """
+
+    def __init__(
+        self,
+        variables: List[str],
+        patterns: Optional[List[str]] = None,
+        hash_length: Optional[int] = None,
+    ):
+        self.variables = variables
+        if not patterns:
+            # there are no patterns: skip pattern-based masking
+            self.apply = self.dynamic_apply
+            self.hash_length = hash_length
+        else:
+            super().__init__(patterns, hash_length)
+
+    def dynamic_apply(self, text: str) -> str:
+        """
+        Return a string with all environment variable values in `text` masked
+        """
+        combined = self.combine(
+            value
+            for variable in self.variables
+            if (value := os.environ.get(variable)) is not None
+        )
+        if not combined:
+            return text
+        return re.sub(combined, self.mask, text)
+
+    def apply(self, text: str) -> str:
+        """
+        Return a string with all pattern matches in `text` masked
+        (including occurences of specified environment variable values)
+        """
+        return self.dynamic_apply(super().apply(text))
--- agent/testflinger_agent/tests/test_masker.py
+++ agent/testflinger_agent/tests/test_masker.py
@@ -1,15 +1,15 @@
-from testflinger_agent.masking import Masker
+import os
+from testflinger_agent.masking import Masker, DynamicMasker
 
 
 def test_hash_consistency():
-    hash_length = 12
-    masker = Masker([], hash_length=hash_length)
     text = "123 testflinger-456 no-secret"
     # hash same text twice and compare hashes
-    hash1 = masker.hash(text)
-    hash2 = masker.hash(text)
+    hash1 = Masker.hash(text)
+    hash2 = Masker.hash(text)
     assert hash1 == hash2
-    assert len(hash1) == hash_length
+    # expected hash length is the full SHA-256 hash, i.e. 64 characters
+    assert len(hash1) == 64
 
 
 def test_no_matches():
@@ -19,17 +19,19 @@ def test_no_matches():
     text = "testflinger"
     # there should be no masking
     masked = masker.apply(text)
+    print(masked)
     assert masked == text
 
 
 def test_no_hash_length():
     # masker for numerical values, no hash length
-    # (expected hash length is the full SHA-256 hash, i.e. 64 characters)
     masker = Masker([r"\d+"])
     # input text is numerical
     text = "123456"
-    hash_value = masker.hash(text)
-    assert len(hash_value) == 64
+    masked = masker.apply(text)
+    print(masked)
+    # expected hash length is the full SHA-256 hash, i.e. 64 characters
+    assert len(masked) == 64 + 4
 
 
 def test_mask_format():
@@ -48,6 +50,7 @@ def test_apply_with_single_pattern():
     text = "SSN: 123-45-6789"
     masker = Masker([r"\b\d{3}-\d{2}-\d{4}\b"], hash_length=hash_length)
     masked = masker.apply(text)
+    print(masked)
     assert "123-45-6789" not in masked
     assert masked.startswith("SSN: **")
     assert masked.endswith("**")
@@ -64,6 +67,39 @@ def test_apply_with_multiple_matches():
         hash_length=hash_length,
     )
     masked = masker.apply(text)
+    print(masked)
+    assert "john@example.com" not in masked
+    assert "123-45-6789" not in masked
+    assert masked.count("**") == 4
+
+
+def test_dynamic_with_pattern():
+    hash_length = 12
+    text = "Contact: john@example.com, SSN: 123-45-6789"
+    os.environ["EMAIL"] = "john@example.com"
+    masker = DynamicMasker(
+        variables=["EMAIL"],
+        patterns=[r"\b\d{3}-\d{2}-\d{4}\b"],
+        hash_length=hash_length,
+    )
+    masked = masker.apply(text)
+    print(masked)
+    assert "john@example.com" not in masked
+    assert "123-45-6789" not in masked
+    assert masked.count("**") == 4
+
+
+def test_dynamic_without_pattern():
+    hash_length = 12
+    text = "Contact: john@example.com, SSN: 123-45-6789"
+    os.environ["EMAIL"] = "john@example.com"
+    os.environ["SSN"] = "123-45-6789"
+    masker = DynamicMasker(
+        variables=["EMAIL", "SSN"],
+        hash_length=hash_length,
+    )
+    masked = masker.apply(text)
+    print(masked)
     assert "john@example.com" not in masked
     assert "123-45-6789" not in masked
     assert masked.count("**") == 4
