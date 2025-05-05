bc3288b52b9114ec21aa9e356a1daaca3522f336
Varun Valada <varun.valada@gmail.com>
2025-05-05 16:16:24 -0500

    Added phase querying to GET endpoint

--- server/tests/test_logging.py
+++ server/tests/test_logging.py
@@ -20,8 +20,9 @@ import uuid
 from datetime import datetime, timezone
 
 import pytest
+from testflinger_common.enums import LogType, TestPhase
 
-from testflinger.log_handlers import LogType, MongoLogHandler
+from testflinger.log_handlers import MongoLogHandler
 
 
 @pytest.fixture(name="mongo_app_with_outputs")
@@ -31,9 +32,11 @@ def fixture_mongo_app_with_outputs(mongo_app):
     app, mongo = mongo_app
     for i in range(10):
         # Add fragments with timestamp staggered by 5 minutes
-        mongo.db.logs.output_fragments.insert_one(
+        mongo.db.logs.insert_one(
             {
                 "job_id": job_id,
+                "log_type": str(LogType.STANDARD_OUTPUT),
+                "phase": TestPhase.SETUP,
                 "fragment_number": i,
                 "timestamp": datetime(
                     2025, 4, 24, 10, 5 * i, 0, tzinfo=timezone.utc
@@ -51,13 +54,14 @@ def test_store_log_fragment(mongo_app):
     log_handler = MongoLogHandler(mongo)
     for i in range(2):
         log_data = {
+            "phase": TestPhase.SETUP,
             "fragment_number": i,
             "log_data": f"My log data {i}",
         }
-        log_handler.store_log_fragment(job_id, log_data, LogType.NORMAL_OUTPUT)
-    log_fragments = list(
-        mongo.db.logs.output_fragments.find({"job_id": job_id})
-    )
+        log_handler.store_log_fragment(
+            job_id, log_data, LogType.STANDARD_OUTPUT
+        )
+    log_fragments = list(mongo.db.logs.find({"job_id": job_id}))
     assert len(log_fragments) == 2
     assert log_fragments[0]["fragment_number"] == 0
     assert log_fragments[0]["log_data"] == "My log data 0"
@@ -71,7 +75,7 @@ def test_retrieve_log_fragments(mongo_app_with_outputs):
     log_handler = MongoLogHandler(mongo)
     start_fragment = 5
     fragments = log_handler.retrieve_log_fragments(
-        job_id, LogType.NORMAL_OUTPUT, start_fragment
+        job_id, LogType.STANDARD_OUTPUT, TestPhase.SETUP, start_fragment
     )
     assert len(fragments) == 5
     for i, f in enumerate(fragments):
@@ -88,7 +92,10 @@ def test_retrieve_log_fragments_by_timestamp(mongo_app_with_outputs):
     log_handler = MongoLogHandler(mongo)
     start_timestamp = datetime(2025, 4, 24, 10, 32, 0, tzinfo=timezone.utc)
     fragments = log_handler.retrieve_log_fragments(
-        job_id, LogType.NORMAL_OUTPUT, start_timestamp=start_timestamp
+        job_id,
+        LogType.STANDARD_OUTPUT,
+        TestPhase.SETUP,
+        start_timestamp=start_timestamp,
     )
     assert len(fragments) == 3
     for i, f in enumerate(fragments):
@@ -100,7 +107,9 @@ def test_retrieve_logs(mongo_app_with_outputs):
     """Tests that combined logs can be retrieved using the log_handler."""
     _, mongo, job_id = mongo_app_with_outputs
     log_handler = MongoLogHandler(mongo)
-    combined_log = log_handler.retrieve_logs(job_id, LogType.NORMAL_OUTPUT, 0)
+    combined_log = log_handler.retrieve_logs(
+        job_id, LogType.STANDARD_OUTPUT, TestPhase.SETUP, 0
+    )
     combined_log_expected = "".join([f"My log data {i}" for i in range(10)])
     assert combined_log["last_fragment_number"] == 9
     assert combined_log["log_data"] == combined_log_expected
