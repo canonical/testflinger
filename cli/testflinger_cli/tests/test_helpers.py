143f4df2631211fa0c912a6a219e986f0fd23de3
Pedro Avalos Jimenez <pedro.avalosjimenez@canonical.com>
2025-05-05 20:36:24 -0400

    Add tests

---
+++ cli/testflinger_cli/tests/test_helpers.py
@@ -0,0 +1,53 @@
+# Copyright (C) 2025 Canonical Ltd.
+"""Tests for the helpers of Testflinger CLI."""
+
+from pathlib import Path
+
+import pytest
+
+from testflinger_cli.consts import SNAP_NAME
+from testflinger_cli.helpers import (
+    file_is_in_snap_private_dir,
+    is_snap,
+    parse_filename,
+)
+
+
+def test_is_snap(monkeypatch):
+    """Test the is_snap function."""
+    monkeypatch.setenv("SNAP_NAME", "")
+    assert not is_snap()
+
+    monkeypatch.setenv("SNAP_NAME", SNAP_NAME)
+    assert is_snap()
+
+
+def test_is_in_snap_private_dir(monkeypatch):
+    """Test the is_in_snap_private_dir function."""
+    monkeypatch.setenv("SNAP_NAME", SNAP_NAME)
+
+    assert file_is_in_snap_private_dir(Path("/tmp/job.yaml"))
+    assert not file_is_in_snap_private_dir(Path("/home/ubuntu/tmp/job.yaml"))
+
+
+def test_parse_filename_snap_private(monkeypatch):
+    """Test the parse_filename function."""
+    # Raises exception if the file is in a snap-confined directory
+    monkeypatch.setenv("SNAP_NAME", SNAP_NAME)
+    with pytest.raises(ValueError):
+        parse_filename("/tmp/job.yaml", check_snap_private_dir=True)
+
+    # Test that you can override the check_snap_private_dir
+    assert parse_filename(
+        "/tmp/job.yaml", check_snap_private_dir=False
+    ) == Path("/tmp/job.yaml")
+
+    # Test that normal files are not affected
+    assert parse_filename(
+        "/home/ubuntu/tmp/job.yaml", check_snap_private_dir=True
+    ) == Path("/home/ubuntu/tmp/job.yaml")
+
+
+def test_parse_filename_stdin():
+    """Test the parse_filename function with stdin."""
+    assert parse_filename("-", parse_stdin=True) is None
