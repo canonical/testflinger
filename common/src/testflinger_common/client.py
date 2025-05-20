3eb5052942ffe3955da69b9c1dcb7b1e43239179
Pedro Avalos Jimenez <pedro.avalosjimenez@canonical.com>
2025-05-20 10:19:33 -0400

    Use common get and post functions

--- common/src/testflinger_common/client.py
+++ common/src/testflinger_common/client.py
@@ -26,6 +26,7 @@ class Client:
         client_secret: Optional[str] = None,
         retries: int = 3,
     ) -> None:
+        """Initialize the client."""
         self.server = server
         self.client_id = client_id
         self.client_secret = client_secret
@@ -54,10 +55,10 @@ class Client:
         return session
 
     @cached_property
-    def token(self) -> str:
+    def token(self) -> Optional[str]:
         """Return the token for the client."""
         if not self.client_id or not self.client_secret:
-            return ""
+            return None
         url = urllib.parse.urljoin(self.server, "/v1/oauth2/token")
         id_key_pair = f"{self.client_id}:{self.client_secret}"
         encoded_id_key_pair = base64.b64encode(id_key_pair.encode()).decode()
@@ -69,47 +70,41 @@ class Client:
     @cached_property
     def auth_headers(self) -> Optional[dict]:
         """Create an authorization header for the client."""
-        if not self.token:
-            return None
-        return {"Authorization": f"{self.token}"}
+        return {"Authorization": self.token} if self.token else None
 
     def submit_job(self, job_data: dict) -> str:
         """Submit a test job to the server."""
-        url = urllib.parse.urljoin(self.server, "/v1/job")
         headers = self.auth_headers
-        response = self.session.post(url, json=job_data, headers=headers)
-        response.raise_for_status()
+        response = self.post("/v1/job", json=job_data, headers=headers)
         return response.json().get("job_id")
 
     def cancel_job(self, job_id: str) -> None:
         """Cancel a test job on the server."""
         self.post_job_action(job_id=job_id, action="cancel")
 
-    def submit_job_attachment(
-        self, job_id: str, path: Path, timeout_sec: float = 30
+    def submit_job_attachments(
+        self, job_id: str, tarball: Path, timeout_sec: float = 30
     ) -> None:
-        """Submit a test job's attachment to the server."""
+        """Submit a test job's attachments to the server."""
         endpoint = f"/v1/job/{job_id}/attachments"
-        self.post_file(endpoint=endpoint, path=path, timeout_sec=timeout_sec)
+        with tarball.open("rb") as file:
+            files = {"file": (tarball.name, file, "application/x-gzip")}
+            self.post(endpoint, files=files, timeout_sec=timeout_sec)
 
     def get_job_attachments(
         self, job_id: str, path: Path, timeout_sec: float = 600
     ) -> None:
         """Get the attachments of a test job."""
         endpoint = f"/v1/job/{job_id}/attachments"
-        self.get_file(endpoint=endpoint, path=path, timeout_sec=timeout_sec)
+        self.get_file(endpoint, path, timeout_sec=timeout_sec)
 
     def post_job_results(self, job_id: str, data: dict) -> None:
         """Post the results of a test job."""
-        url = urllib.parse.urljoin(self.server, f"/v1/result/{job_id}")
-        response = self.session.post(url, json=data, timeout=30)
-        response.raise_for_status()
+        self.post(f"/v1/result/{job_id}", json=data, timeout_sec=30)
 
     def get_job_results(self, job_id: str) -> dict:
         """Get the results of a test job."""
-        url = urllib.parse.urljoin(self.server, f"/v1/result/{job_id}")
-        response = self.session.get(url)
-        response.raise_for_status()
+        response = self.get(f"/v1/result/{job_id}")
         return response.json()
 
     def get_job_artifacts(self, job_id: str, path: Path) -> None:
@@ -117,27 +112,24 @@ class Client:
         endpoint = f"/v1/result/{job_id}/artifact"
         self.get_file(endpoint=endpoint, path=path)
 
-    def post_job_artifacts(self, job_id: str, path: Path) -> None:
+    def post_job_artifacts(self, job_id: str, tarball: Path) -> None:
         """Post the artifacts of a test job."""
         endpoint = f"/v1/result/{job_id}/artifact"
-        self.post_file(endpoint=endpoint, path=path)
+        with tarball.open("rb") as file:
+            files = {"file": (tarball.name, file, "application/x-gzip")}
+            self.post(endpoint, files=files, timeout_sec=30)
 
     def get_jobs(self, queues: list[str]) -> dict:
         """Get the list of jobs from the server.
 
         :param queues: List of queues to filter jobs by.
         """
-        url = urllib.parse.urljoin(self.server, "/v1/job")
-        params = {"queue": queues}
-        response = self.session.get(url, params=params)
-        response.raise_for_status()
+        response = self.get("/v1/job", params={"queue": queues})
         return response.json()
 
     def get_job(self, job_id: str) -> dict:
         """Get the job definition for a specified ID."""
-        url = urllib.parse.urljoin(self.server, f"/v1/job/{job_id}")
-        response = self.session.get(url)
-        response.raise_for_status()
+        response = self.get(f"/v1/job/{job_id}")
         return response.json()
 
     def get_job_status(self, job_id: str) -> str:
@@ -147,25 +139,16 @@ class Client:
 
     def get_job_position(self, job_id: str) -> int:
         """Get the position of a test job in the queue."""
-        url = urllib.parse.urljoin(self.server, f"/v1/job/{job_id}/position")
-        response = self.session.get(url)
-        response.raise_for_status()
+        response = self.get(f"/v1/job/{job_id}/position")
         return int(response.text)
 
     def post_job_provision_log(
         self, agent_id: str, job_id: str, exit_code: int, detail: str
     ) -> None:
         """Post the outcome of a test job's provision phase."""
-        data = {
-            "job_id": job_id,
-            "exit_code": exit_code,
-            "detail": detail,
-        }
-        url = urllib.parse.urljoin(
-            self.server, f"/v1/agents/provision_logs/{job_id}/{agent_id}"
-        )
-        response = self.session.post(url, json=data, timeout=30)
-        response.raise_for_status()
+        endpoint = f"/v1/agents/provision_logs/{job_id}/{agent_id}"
+        data = {"job_id": job_id, "exit_code": exit_code, "detail": detail}
+        self.post(endpoint, json=data, timeout_sec=30)
 
     def post_job_status_update(
         self,
@@ -184,34 +167,31 @@ class Client:
         :param events: List of accumulated events.
         """
         if not webhook:
-            return
-        url = urllib.parse.urljoin(self.server, f"/v1/job/{job_id}/events")
+            raise ValueError("Webhook URL is required for job status update.")
         data = {
             "agent_id": agent_id,
             "job_queue": job_queue,
             "job_status_webhook": webhook,
             "events": events,
         }
-        response = self.session.post(url, json=data, timeout_sec=30)
-        response.raise_for_status()
+        self.post(f"/v1/job/{job_id}/events", json=data, timeout_sec=30)
 
     def post_job_output(self, job_id: str, output: str) -> None:
         """Post the output of a test job."""
-        url = urllib.parse.urljoin(self.server, f"/v1/job/{job_id}/output")
+        endpoint = f"/v1/job/{job_id}/output"
         data = {"output": output}
-        response = self.session.post(url, json=data, timeout_sec=60)
-        response.raise_for_status()
+        self.post(endpoint, json=data, timeout_sec=60)
 
     def get_job_output(self, job_id: str) -> str:
         """Get the latest output for a test job."""
-        url = urllib.parse.urljoin(self.server, f"/v1/job/{job_id}/output")
-        response = self.session.get(url)
-        response.raise_for_status()
+        response = self.get(f"/v1/job/{job_id}/output")
         return response.text
 
     def get_job_serial_output(self, job_id: str) -> str:
         """Get the latest serial output for a test job."""
-        url = urllib.parse.urljoin(self.server, f"/v1/job/{job_id}/serial_output")
+        url = urllib.parse.urljoin(
+            self.server, f"/v1/job/{job_id}/serial_output"
+        )
         response = self.session.get(url)
         response.raise_for_status()
         return response.text
@@ -221,15 +201,11 @@ class Client:
 
         :param queues: Dictionary of queue name to queue description.
         """
-        url = urllib.parse.urljoin(self.server, "/v1/agents/queues")
-        response = self.session.post(url, json=queues, timeout=30)
-        response.raise_for_status()
+        self.post("/v1/agents/queues", json=queues, timeout_sec=30)
 
     def get_queues(self) -> dict:
         """Get the advertised queues from the server."""
-        url = urllib.parse.urljoin(self.server, "/v1/agents/queues")
-        response = self.session.get(url)
-        response.raise_for_status()
+        response = self.get("/v1/agents/queues")
         return response.json()
 
     def post_queue_images(
@@ -251,31 +227,22 @@ class Client:
                 }
             ```
         """
-        url = urllib.parse.urljoin(self.server, "/v1/agents/images")
-        response = self.session.post(url, json=images)
-        response.raise_for_status()
+        self.post("/v1/agents/images", json=images)
 
     def get_queue_images(self, queue: str) -> dict:
         """Get the advertised images for a queue from the server."""
-        url = urllib.parse.urljoin(self.server, f"/v1/agents/images/{queue}")
-        response = self.session.get(url)
-        response.raise_for_status()
+        response = self.get(f"/v1/agents/images/{queue}")
         return response.json()
 
     def get_queue_agents(self, queue: str) -> dict:
         """Get the list of all agents listening to a specified queue."""
-        url = urllib.parse.urljoin(self.server, f"/v1/queues/{queue}/agents")
-        response = self.session.get(url)
-        response.raise_for_status()
+        response = self.get(f"/v1/queues/{queue}/agents")
         return response.json()
 
     def get_queue_wait_times(self, queues: Optional[list[str]] = None) -> dict:
         """Get the wait times for a list of queues."""
         queues = queues or []
-        url = urllib.parse.urljoin(self.server, "/v1/queues/wait_times")
-        params = {"queue": queues}
-        response = self.session.get(url, params=params)
-        response.raise_for_status()
+        response = self.get("/v1/queues/wait_times", params={"queue": queues})
         return response.json()
 
     def post_agent_data(self, agent_id: str, data: dict) -> None:
@@ -284,16 +251,57 @@ class Client:
         :param agent_id: The ID of the agent.
         :param data: The data to post.
         """
-        url = urllib.parse.urljoin(self.server, f"/v1/agents/data/{agent_id}")
-        response = self.session.post(url, json=data, timeout=30)
-        response.raise_for_status()
+        self.post(f"/v1/agents/data/{agent_id}", json=data, timeout_sec=30)
 
     def post_job_action(self, job_id: str, action: str) -> None:
         """Post an action to a test job."""
-        url = urllib.parse.urljoin(self.server, f"/v1/job/{job_id}/action")
-        data = {"action": action}
-        response = self.session.post(url, json=data)
+        self.post(f"/v1/job/{job_id}/action", json={"action": action})
+
+    def get(
+        self,
+        endpoint: str,
+        headers: Optional[dict] = None,
+        params: Optional[dict] = None,
+        json: Optional[dict] = None,
+        files: Optional[dict] = None,
+        stream: Optional[bool] = None,
+        timeout_sec: float = 15,
+    ) -> requests.Response:
+        """Post data to the server."""
+        url = urllib.parse.urljoin(self.server, endpoint)
+        response = self.session.get(
+            url,
+            headers=headers,
+            params=params,
+            json=json,
+            files=files,
+            stream=stream,
+            timeout=timeout_sec,
+        )
         response.raise_for_status()
+        return response
+
+    def post(
+        self,
+        endpoint: str,
+        headers: Optional[dict] = None,
+        params: Optional[dict] = None,
+        json: Optional[dict] = None,
+        files: Optional[dict] = None,
+        timeout_sec: float = 15,
+    ) -> requests.Response:
+        """Post data to the server."""
+        url = urllib.parse.urljoin(self.server, endpoint)
+        response = self.session.post(
+            url,
+            headers=headers,
+            params=params,
+            json=json,
+            files=files,
+            timeout=timeout_sec,
+        )
+        response.raise_for_status()
+        return response
 
     def get_file(
         self,
@@ -303,19 +311,8 @@ class Client:
         chunk_size: int = 4096,
     ) -> None:
         """Get a file from the server."""
-        uri = urllib.parse.urljoin(self.server, endpoint)
-        response = requests.get(uri, timeout=timeout_sec, stream=True)
-        response.raise_for_status()
+        url = urllib.parse.urljoin(self.server, endpoint)
+        response = self.get(url, timeout_sec=timeout_sec, stream=True)
         with path.open("wb") as file:
             for chunk in response.iter_content(chunk_size=chunk_size):
                 file.write(chunk)
-
-    def post_file(
-        self, endpoint: str, path: Path, timeout_sec: float = 30
-    ) -> None:
-        """Submit a file to the server."""
-        uri = urllib.parse.urljoin(self.server, endpoint)
-        with path.open("rb") as file:
-            files = {"file": (path.name, file, "application/x-gzip")}
-            response = requests.post(uri, files=files, timeout=timeout_sec)
-            response.raise_for_status()
