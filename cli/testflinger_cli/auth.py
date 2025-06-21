23ac9757c4deb0c3c011f6f3d69fdcd41be984fa
rene-oromtz <rene.orozco@canonical.com>
2025-06-20 18:24:05 -0600

    Created class with the generalized methods for testflinger cli authentication

---
+++ cli/testflinger_cli/auth.py
@@ -0,0 +1,111 @@
+# Copyright (C) 2020-2022 Canonical
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""Testflinger CLI Auth module."""
+
+import logging
+import os
+import sys
+from http import HTTPStatus
+from typing import Optional
+
+import jwt
+
+from testflinger_cli import client
+
+logger = logging.getLogger(__name__)
+
+
+class TestflingerCliAuth:
+    """Class to handle authentication and authorisation for Testflinger CLI."""
+
+    def __init__(self, client_id: str, secret_key: str, server):
+        self.client_id = client_id
+        self.secret_key = secret_key
+        self.client = client.Client(server, error_threshold=3)
+        self._jwt_token = None
+        self._authenticate_with_server()
+
+    def is_authenticated(self) -> bool:
+        """Validate if user is currently authenticated.
+
+        :return: Status for user authentication.
+        """
+        return self._jwt_token is not None
+
+    def _authenticate_with_server(self) -> None:
+        """
+        Authenticate client id and secret key with server
+        and store JWT with permissions.
+        """
+        if self.client_id is None or self.secret_key is None:
+            return None
+
+        try:
+            self._jwt_token = self.client.authenticate(
+                self.client_id, self.secret_key
+            )
+        except client.HTTPError as exc:
+            if exc.status == HTTPStatus.UNAUTHORIZED:
+                sys.exit(
+                    "Authentication with Testflinger server failed. "
+                    "Check your client id and secret key"
+                )
+            elif exc.status == HTTPStatus.FORBIDDEN:
+                sys.exit(
+                    "Received 403 error from server."
+                    "Confirm you are connected to the right network."
+                )
+
+    def build_auth_headers(self) -> Optional[dict]:
+        """Create an authorization header based on stored JWT.
+
+        :return: Dict with JWT as the Authorization header if exist.
+        """
+        return {"Authorization": self._jwt_token} if self._jwt_token else None
+
+    def decode_jwt_token(self) -> Optional[dict]:
+        """Decode authorization token using the secret key.
+
+        :return: Dict with the decoded JWT
+        """
+        secret_key = os.environ.get("JWT_SIGNING_KEY")
+        if self.jwt_token and secret_key:
+            try:
+                decoded_jwt = jwt.decode(
+                    self.jwt_token,
+                    secret_key,
+                    algorithms="HS256",
+                    options={"require": ["exp", "iat", "sub"]},
+                )
+                return decoded_jwt
+            except jwt.exceptions.ExpiredSignatureError:
+                logger.warning("Token has expired")
+            except jwt.exceptions.InvalidTokenError:
+                logger.warning("Invalid Token Provided")
+        return None
+
+    def get_user_role(self) -> str:
+        """Retrieve the role for the user from the decoded jwt.
+
+        :return: String with the role, defaults to 'user'
+        """
+        decoded_token = self.decode_jwt_token()
+        if not decoded_token:
+            return "user"
+
+        permissions = decoded_token.get("permissions", {})
+        return permissions.get("role", "user")
