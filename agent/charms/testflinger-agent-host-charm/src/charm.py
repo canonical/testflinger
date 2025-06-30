5b9bf199487f5cb8da48a5e081d65ee139a629ba
Varun Valada <varun.valada@gmail.com>
2025-06-30 16:03:17 -0500

    Add grafana agent relation

--- agent/charms/testflinger-agent-host-charm/src/charm.py
+++ agent/charms/testflinger-agent-host-charm/src/charm.py
@@ -30,7 +30,10 @@ from ops.model import (
 )
 
 from charms.operator_libs_linux.v0 import apt
-from charms.prometheus_k8s.v0.prometheus_scrape import MetricsEndpointProvider
+from charms.prometheus_k8s.v0.prometheus_scrape import (
+    MetricsEndpointProvider
+)
+from charms.grafana_agent.v0.cos_agent import COSAgentProvider
 
 logger = logging.getLogger(__name__)
 
@@ -52,7 +55,12 @@ class TestflingerAgentHostCharm(CharmBase):
             self.on.update_testflinger_action,
             self.on_update_testflinger_action,
         )
-        self.metrics_endpoint = MetricsEndpointProvider(self)
+        self.scrape_jobs = []
+        self.metrics_endpoint = MetricsEndpointProvider(
+            self,
+            self.get_scrape_jobs,
+        )
+        self._grafana_agent = COSAgentProvider(self)
 
     def on_install(self, _):
         """Install hook."""
@@ -134,7 +142,7 @@ class TestflingerAgentHostCharm(CharmBase):
             shutil.rmtree(repo_path, ignore_errors=True)
         shutil.move(tmp_repo_path, repo_path)
 
-    def write_supervisor_service_files(self):
+    def write_supervisor_service_files(self, initial_metrics_port=8000):
         """
         Generate supervisord service files for all agents.
 
@@ -174,8 +182,8 @@ class TestflingerAgentHostCharm(CharmBase):
         ) as service_template:
             template = Template(service_template.read())
 
-        metric_endpoint_port = 8000
-        prometheus_jobs = []
+        metric_endpoint_port = initial_metrics_port
+        self.scrape_jobs = []
         for agent_dir in agent_dirs:
             agent_config_path = agent_dir
             agent_name = agent_dir.name
@@ -185,7 +193,7 @@ class TestflingerAgentHostCharm(CharmBase):
                 virtual_env_path=VIRTUAL_ENV_PATH,
                 metric_endpoint_port=metric_endpoint_port,
             )
-            prometheus_jobs.append(
+            self.scrape_jobs.append(
                 {
                     "job_name": agent_name,
                     "static_configs": [
@@ -198,7 +206,7 @@ class TestflingerAgentHostCharm(CharmBase):
                 f"/etc/supervisor/conf.d/{agent_name}.conf", "w"
             ) as agent_file:
                 agent_file.write(rendered)
-        self.metrics_endpoint.update_scrape_job_spec(prometheus_jobs)
+        self.metrics_endpoint.update_scrape_job_spec(self.scrape_jobs)
 
     def supervisor_update(self):
         """
@@ -337,6 +345,8 @@ class TestflingerAgentHostCharm(CharmBase):
         self.restart_agents()
         self.unit.status = ActiveStatus()
 
+    def get_scrape_jobs(self):
+        return self.scrape_jobs
 
 if __name__ == "__main__":
     main(TestflingerAgentHostCharm)
