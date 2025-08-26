f853b9994fa1393f373157f1d1fc6f8c165aadd5
rene-oromtz <rene.orozco@canonical.com>
2025-08-26 16:57:19 -0600

    fix(agent): send a heartbeat to server at least once per day

--- agent/src/testflinger_agent/handlers.py
+++ agent/src/testflinger_agent/handlers.py
@@ -12,6 +12,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
+from datetime import UTC, datetime
+
 from .client import TestflingerClient
 
 
@@ -82,3 +84,60 @@ class AgentStatusHandler:
     def comment(self) -> str:
         """Retrieve the comment on the state."""
         return self._comment
+
+class AgentHeartbeatHandler:
+    """Handler to determine if agent needs to send a heartbeat signal."""
+
+    def __init__(self, client: TestflingerClient, heartbeat_frequency: int):
+        """Initialize handler with default values."""
+        self.client = client
+        self.heartbeat_frequency = heartbeat_frequency
+        self._agent_data = {}
+        # Agent sent heartbeat upon initialization or restart
+        self._last_heartbeat = datetime.now(UTC)
+
+    def update(self, agent_data: dict):
+        """Update agent_data retrieved from server.
+
+        :param agent_data: Agent information retrieved from server.
+        """
+        if agent_data:
+            self._agent_data = agent_data
+            self._refresh_last_heartbeat()
+            self._send_heartbeat()
+
+    def _refresh_last_heartbeat(self) -> None:
+        """Update heartbeat from agent_data.
+
+        :param agent_data: All agent information retrieved from server.
+        """
+        if "updated_at" in self._agent_data:
+            # Parse ISO format timestamp
+            timestamp = self._agent_data["updated_at"]
+            self._last_heartbeat = datetime.fromisoformat(
+                timestamp.replace("Z", "+00:00")
+            )
+
+    def _send_heartbeat(self) -> None:
+        """Send a heartbeat to the server if needed.
+
+        Heartbeat signal is current agent status and comment.
+        """
+        if self.is_heartbeat_required():
+            comment = self._agent_data.get("comment", "")
+            if "state" in self._agent_data:
+                agent_state = self._agent_data["state"]
+                self.client.post_agent_data(
+                    {"state": agent_state, "comment": comment}
+                )
+
+    def is_heartbeat_required(self) -> bool:
+        """Determine if heartbeat is required to send to server.
+
+        :return: True or False if heartbeat is required
+        """
+        time_delta = datetime.now(UTC) - self._last_heartbeat
+        # Heartbeat is required at least once per heartbeat frequency
+        if time_delta.days >= self.heartbeat_frequency:
+            return True
+        return False
