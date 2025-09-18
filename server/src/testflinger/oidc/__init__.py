c0e4069650fdd6f3163456e13b2c06a47d90dad6
rene-oromtz <rene.orozco@canonical.com>
2025-09-18 15:42:08 -0600

    feat(server): add oidc routes and sign in button

---
+++ server/src/testflinger/oidc/__init__.py
@@ -0,0 +1,62 @@
+# Copyright (C) 2025 Canonical
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+
+from apiflask import APIFlask
+from authlib.integrations.flask_client import OAuth
+
+
+def setup_oidc_config(tf_app: APIFlask):
+    """Define OIDC configuration for web authentication.
+
+    :param tf_app: Testflinger app to set configuration
+    """
+    tf_app.config["SECRET_KEY"] = os.environ.get("WEB_SECRET_KEY")
+    tf_app.config["OIDC_CLIENT_ID"] = os.environ.get("OIDC_CLIENT_ID")
+    tf_app.config["OIDC_CLIENT_SECRET"] = os.environ.get("OIDC_CLIENT_SECRET")
+    tf_app.config["OIDC_PROVIDER_DOMAIN"] = os.environ.get(
+        "OIDC_PROVIDER_DOMAIN"
+    )
+
+
+def app_register_oidc(tf_app: APIFlask) -> OAuth | None:
+    """Register app with OIDC if provider is set.
+
+    :param tf_app: Testflinger app to register with OIDC
+    """
+    setup_oidc_config(tf_app=tf_app)
+
+    client_id = tf_app.config["OIDC_CLIENT_ID"]
+    client_secret = tf_app.config["OIDC_CLIENT_SECRET"]
+    domain = tf_app.config["OIDC_PROVIDER_DOMAIN"]
+    secret_key = tf_app.config["SECRET_KEY"]
+
+    # If provider is not set, app will run without web authentication
+    if not all([client_id, client_secret, domain, secret_key]):
+        return None
+
+    # Register app with OID based on configuration values
+    oauth = OAuth(tf_app)
+    oauth.register(
+        "oidc",
+        client_id=client_id,
+        client_secret=client_secret,
+        client_kwargs={
+            "scope": "openid profile email",
+        },
+        server_metadata_url=f"{domain}/.well-known/openid-configuration",
+    )
+    return oauth
---
+++ server/src/testflinger/oidc/views.py
@@ -0,0 +1,52 @@
+# Copyright (C) 2023-2024 Canonical
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""OIDC related views for authenticated application."""
+
+from flask import (
+    Blueprint,
+    current_app,
+    redirect,
+    session,
+    url_for,
+)
+
+oidc_views = Blueprint("oidc", __name__)
+
+@oidc_views.route("/callback")
+def callback():
+    """Redirect callback from OIDC Provider."""
+    token = current_app.oauth.oidc.authorize_access_token()
+    session["user"] = token
+    return redirect(url_for("testflinger.home"))
+
+
+@oidc_views.route("/login")
+def login():
+    """Redirects user to OIDC provider for authentication."""
+    if not current_app.oauth:
+        # OIDC not configured - redirect to home or show error
+        return redirect(url_for("testflinger.home"))
+
+    return current_app.oauth.oidc.authorize_redirect(
+        redirect_uri=url_for("oidc.callback", _external=True)
+    )
+
+
+@oidc_views.route("/logout")
+def logout():
+    """Terminate user session and from the OIDC provider."""
+    session.clear()
+    return redirect(url_for("testflinger.home"))
