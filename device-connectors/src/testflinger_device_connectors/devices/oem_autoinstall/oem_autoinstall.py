7c7cc14b33163dd53de51784c59eea7cc97e2341
Artur Pak <artur.pak@canonical.com>
2026-02-05 13:28:46 +0800

    style: Fix ruff

--- device-connectors/src/testflinger_device_connectors/devices/oem_autoinstall/oem_autoinstall.py
+++ device-connectors/src/testflinger_device_connectors/devices/oem_autoinstall/oem_autoinstall.py
@@ -30,7 +30,6 @@ import yaml
 
 from testflinger_device_connectors.devices import (
     ProvisioningError,
-    RecoveryError,
 )
 
 logger = logging.getLogger(__name__)
@@ -227,7 +226,7 @@ class OemAutoinstall:
             try:
                 subprocess.check_call(cmd.split(), timeout=120)
             except subprocess.SubprocessError as exc:
-                raise ProvisioningError("Error running reboot script!") from exc
+                raise ProvisioningError("Error running reboot script") from exc
 
     def check_device_booted(self):
         """Check to see if the device is booted and reachable with ssh."""
