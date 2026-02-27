708439187e111bfe55b3c097d626358f3e06aedc
Rene Orozco Martinez <rene.orozco@canonical.com>
2026-02-27 16:21:35 -0700

    typo fixes

--- agent/terraform/dev/main.tf
+++ agent/terraform/dev/main.tf
@@ -1,7 +1,7 @@
 module "agent-host" {
   source               = "git::https://github.com/canonical/testflinger.git//agent/terraform?ref=feat/update-terraform"
   app_name             = "agent-host-test"
-  model_uuid           = data.juju_model.agent-host-model.uuid
+  model_uuid           = data.juju_model.agent_host_model.uuid
   constraints          = "arch=amd64 cores=1 mem=2048M root-disk=10240M root-disk-source=default virt-type=virtual-machine"
   units                = 1
   base                 = "ubuntu@22.04"
@@ -13,7 +13,7 @@ module "agent-host" {
   config = {
     "config-branch"      = "main"
     "config-dir"         = "agent/charms/testflinger-agent-host-charm/tests/integration/data/test01"
-    "credentials-secret" = "secret:${juju_secret.credentials-secret.secret_id}"
+    "credentials-secret" = "secret:${juju_secret.credentials_secret.secret_id}"
     "testflinger-server" = "https://testflinger.canonical.com"
   }
 }
