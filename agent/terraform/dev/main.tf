21d5afe2523f0045ef6e4f9531b2311313677688
Rene Orozco Martinez <rene.orozco@canonical.com>
2026-02-27 17:03:57 -0700

    dev: point to local dir for testing

--- agent/terraform/dev/main.tf
+++ agent/terraform/dev/main.tf
@@ -1,5 +1,5 @@
 module "agent-host" {
-  source               = "git::https://github.com/canonical/testflinger.git//agent/terraform?ref=feat/update-terraform"
+  source               = "../"
   app_name             = "agent-host-test"
   model_uuid           = data.juju_model.agent_host_model.uuid
   constraints          = "arch=amd64 cores=1 mem=2048M root-disk=10240M root-disk-source=default virt-type=virtual-machine"
