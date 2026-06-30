2475440b7ff04943e548e6959864c0d455a5f607
Andy Zobro <241730435+ajzobro@users.noreply.github.com>
2026-06-30 15:57:06 -0500

    Update server/src/testflinger/api/v1.py

--- server/src/testflinger/api/v1.py
+++ server/src/testflinger/api/v1.py
@@ -566,7 +566,7 @@ def action_post(job_id, json_data):
     :param job_id:
         UUID as a string for the job
     """
-    # TODO: limit to job owner (or greater) if auth enabled (so job owner known
+    # TODO: limit to job owner (or greater) if auth enabled so job owner known
     if not check_valid_uuid(job_id):
         return "Invalid job id\n", 400
     action = json_data["action"]
