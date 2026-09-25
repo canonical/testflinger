be6d40c5dae05faaae2c93423880a2e870fe1a43
ajzobro <241730435+ajzobro@users.noreply.github.com>
2026-09-25 13:29:25 -0500

    move v2 changes into create sample data now that the enums and api exist

--- server/devel/sample_users.py
+++ server/devel/sample_users.py
@@ -18,6 +18,8 @@ Sample user data for use in local testing and development.
 # This is the single handwritten source for local accounts. Entries with a
 # dex_user_id are rendered into dex-config.yaml. Entries with dev_auto_signin
 # are also rendered into testflinger.dev_signin_identities.
+TESTFLINGER_AGENT_ID = "testflinger-sample-agent"
+TESTFLINGER_AGENT_SECRET = "testflinger"  # noqa: S105
 SAMPLE_CLIENTS = [
     {
         "client_id": "testflinger-admin",
